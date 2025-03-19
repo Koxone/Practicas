7332030da32563dc4d4c1689f2ea873ef2157a9c
Koxone <admin@kooxland.com>
2025-03-19 15:04:33 -0600

    v3.0

--- ticTacToeGameV2.0/index.js
+++ ticTacToeGameV2.0/index.js
@@ -1,60 +1,70 @@
 //Variables Globales
-let playerOneSelection = undefined; //True means X, False means O
+let playerOneSelection = undefined; 
+let playerCPUSelection = true;
 
 //Funcion para seleccion de Jugador 1
 function selectPlayer() {
-    const xUnselected = document.getElementById('xUnselected');
-    const oUnselected = document.getElementById('oUnselected');
-    const toggleShape = document.getElementById('toggleShape');
+  const xUnselected = document.getElementById('xUnselected')
+  const oUnselected = document.getElementById('oUnselected')
+  const toggleShape = document.getElementById('toggleShape')
 
-    xUnselected.addEventListener('click', function() {
-        const shapeValue = getComputedStyle(toggleShape);
-        const xSelected = document.getElementById('xSelected');
-        const oSelected = document.getElementById('oSelected');
+  xUnselected.addEventListener('click', function () {
+    const shapeValue = getComputedStyle(toggleShape)
+    const xSelected = document.getElementById('xSelected')
+    const oSelected = document.getElementById('oSelected')
 
-        if (shapeValue.right === '11.1562px') {
-            console.log('Shape estaba en O');
-            toggleShape.classList.add('active');
-            xUnselected.style.display = 'none';
-            xSelected.style.display = 'block';
-            xSelected.style.zIndex = '2';
-            oUnselected.style.display = 'block';
-            oSelected.style.display = 'none';
-            playerOneSelection = true;
-        }
-    });
+    if (shapeValue.right === '11.1562px') {
+      console.log('Shape estaba en O')
+      toggleShape.classList.add('active')
+      xUnselected.style.display = 'none'
+      xSelected.style.display = 'block'
+      xSelected.style.zIndex = '2'
+      oUnselected.style.display = 'block'
+      oSelected.style.display = 'none'
+      playerOneSelection = true
+      playerCPUSelection = false
+    }
+  })
 
-    oUnselected.addEventListener('click', function() {
-        const shapeValue = getComputedStyle(toggleShape);
-        const xSelected = document.getElementById('xSelected');
-        const oSelected = document.getElementById('oSelected');
+  oUnselected.addEventListener('click', function () {
+    const shapeValue = getComputedStyle(toggleShape)
+    const xSelected = document.getElementById('xSelected')
+    const oSelected = document.getElementById('oSelected')
 
-        if (shapeValue.right === '133.906px') {
-            console.log('Shape estaba en X');
-            toggleShape.classList.remove('active');
-            xUnselected.style.display = 'block';
-            xSelected.style.display = 'none';
-            xSelected.style.zIndex = '2';
-            oUnselected.style.display = 'none';
-            oSelected.style.display = 'block';
-            playerOneSelection = false;
-        }
-    });
+    if (shapeValue.right === '133.906px') {
+      console.log('Shape estaba en X')
+      toggleShape.classList.remove('active')
+      xUnselected.style.display = 'block'
+      xSelected.style.display = 'none'
+      xSelected.style.zIndex = '2'
+      oUnselected.style.display = 'none'
+      oSelected.style.display = 'block'
+      playerOneSelection = false
+      playerCPUSelection = true
+    }
+  })
 }
-selectPlayer();
-
-document.addEventListener('click', function() {
-    let prueba = localStorage.getItem('playerOneSelection');
-    console.log(playerOneSelection);
-});
+selectPlayer()
 
 //Funcion para iniciar el juego en vsPlayer
-function vsPlayer() {
-    const vsPlayerButton = document.getElementById('newGameCPUButton');
-
-    vsPlayerButton.addEventListener('click', function() {
-        localStorage.setItem('playerOneSelection', playerOneSelection);
-        window.location.href = 'boardGame.html';
-    });
+const vsPlayer = () => {
+    const vsPlayerButton = document.getElementById('newGameCPUButton')
+    vsPlayerButton.addEventListener('click', () => {
+        localStorage.setItem('playerOneSelection', playerOneSelection)
+        localStorage.setItem('gameMode', 'player')
+        window.location.href = 'boardGame.html'
+    })
 }
-vsPlayer();
+vsPlayer()
+
+//Funcion para iniciar el juego en vsCPU
+const vsCPU = () => {
+    const vsCpuButton = document.getElementById('newGamePlayerButton')
+    vsCpuButton.addEventListener('click', () => {
+        localStorage.setItem('playerOneSelection', playerOneSelection)
+        localStorage.setItem('playerCPUSelection', playerCPUSelection)
+        localStorage.setItem('gameMode', 'cpu')
+        window.location.href = 'boardGame.html'
+    })
+}
+vsCPU()
